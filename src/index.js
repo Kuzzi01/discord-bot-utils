29d3640057eedf80c885ae10ba793f116eec6986
Kuzzi <86385569+Kuzzi01@users.noreply.github.com>
2021-07-04 23:08:51 -0400

    [add] nueva funcionalidad progressBar()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const utils = require('./utils')
 
 /**
  * Genera un codigo random de la cantidad de characters que desees __Recomendado__: 16 o 32
@@ -125,6 +126,40 @@ return str.replace(/2&#(\d+)k/g, function(match, dec) {
 })
 };
 
+/**
+ * Crea una barra de progreso asignando el porcentaje `total` y el porcentaje que lleva hasta ahora `current`.
+ * @param {number} total El total de la porcentaje de la barra, normalmente 100 aunque puede cambiarse [Obligatorio]
+ * @param {number} current La cantidad de progreso [Obligatorio]
+ * @param {number} size El tamaño de la barra de progreso *por defecto 10* (Opcional)
+ * @param {string} line El simbolo del background de la barra; *por defecto □* (Opcional)
+ * @param {string} slider El simbolo de carga de la barra; *por defecto ■* (Opcional)
+ * @returns obj
+ */
+ function progressBar(total, current, size = 10, line = '□', slider = '■'){    
+	if (current > total) {
+
+		const bar = slider.repeat(size + 2);
+		const percentage = (current / total) * 100;
+    let objeto = new utils.obj(bar, percentage)
+
+		return objeto;
+
+	} else {
+
+		const percentage = current / total;
+		const progress = Math.round((size * percentage));
+		const emptyProgress = size - progress;
+		const progressText = slider.repeat(progress);
+		const emptyProgressText = line.repeat(emptyProgress);
+		const bar = progressText + emptyProgressText;
+		const calculated = percentage * 100;
+    let objeto = new utils.obj(bar, calculated)
+
+		return objeto
+
+	}
+};
+
 module.exports = {
   randomCode,
   ms,
@@ -133,5 +168,6 @@ module.exports = {
   validMail,
   discordInv,
   encode,
-  decode
+  decode,
+  progressBar
 }
---
+++ src/utils.js
@@ -0,0 +1,14 @@
+
+/**
+ * Constructor para generar un objeto de la barra
+ * @param {number} barra La barra
+ * @param {number} porcentaje El porcentaje llenado de la barra
+ */
+function obj(barra, porcentaje){
+    this.barra = barra
+    this.porcentaje = porcentaje
+}
+
+module.exports = {
+    obj
+}
